07b69edb26ce867e76757f276690895c6131d0a5
Bhaskar Anand <85471852+BhaskarAnand-05@users.noreply.github.com>
2022-01-18 16:07:20 +0530

    Adding Pattern Questions

---
+++ Pattern.cpp
@@ -0,0 +1,322 @@
+#include "Headerfile.h"
+
+int main()
+{
+    int n;
+    cin >> n;
+    // int n2;
+    // cin>>n2;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     int count=0;
+    //     for (int j = 0; j < i; j++)
+    //     {
+    //         cout<<" ";
+
+    //     }for (int k = n; k > i; k--)
+    //         {
+    //             cout << ++count;
+    //         }
+    //     cout<<"\n";
+
+    // }
+    //  cout<<"\n\n";
+    // // 2.
+
+    // for (int  i = 0; i < n; i++)
+    // {
+    //     for (int j = n; j > i; j--)
+    //     {
+    //         cout<<" ";
+    //     }
+    //     for (int k = 0; k <= i; k++)
+    //     {
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+
+    // }
+    // cout<<"\n\n";
+    // // 3.
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j <= i; j++)
+    //     {
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+    // cout<<"\n\n";
+    // // 4.@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@IMPORTANT
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = (2*n-i); j > i ; j--)
+    //     {
+    //         cout<<" ";
+    //     }
+    //     for (int k = 0; k < i; k++)
+    //     {
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+
+    // }cout<<"\n\n";
+
+    // // 5.
+
+    // // 
+
+    // // 6.@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@IMPORTANT
+
+    // for (int i = 0; i <= n; i++)
+    // {
+    //     for (int j = 0; j <= n2; j++)
+    //     {
+
+    //         if (i==0 || i==n)
+    //         {
+    //             cout << "* " ;
+    //         }
+
+    //         else if(j==0 || j==n2)
+    //         {
+    //             cout<<"* ";
+    //         }
+    //         else
+    //         {
+    //             cout<<"  ";
+    //         }
+
+    //     }
+    //     cout<<endl;
+
+    // }cout<<"\n\n";
+
+    // // 7.
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     char ch = 'A';
+    //     for (int j = 0; j < i; j++)
+    //     {
+    //         cout<<ch++<<" ";
+    //     }
+    //     cout<<endl;
+    // }cout<<"\n\n";
+
+    // // 8.
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = n; j > i; j--)
+    //     {
+    //         cout<<" ";
+    //     }
+    //     for (int k = 0; k <= i; k++)
+    //     {
+    //         if (k == 0 || k==i)
+    //         {
+    //             cout << "& ";
+    //         }
+    //         else
+    //         {
+    //             cout<<"  ";
+    //         }
+
+    //     }
+    //     cout<<endl;
+
+    // }
+    // for (int i = 0; i <(n+1); i++)
+    // {
+    //     cout << "& ";
+
+    // }cout<<"\n\n";
+
+    // //9.
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < i; j++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     for (int k = n; k > i; k--)
+    //     {
+    //         cout << "@ ";
+    //     }
+    //     cout << endl;
+    // }cout<<"\n\n";
+
+    // //10. Right Pascals Triangle
+    
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < i; j++)
+    //     {
+    //         cout<<"@ ";
+    //     }
+    //     cout<<endl;
+        
+    // }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = n; j > i; j--)
+    //     {
+    //         cout<<"@ ";
+    //     }
+    //     cout<<endl;
+    // }cout<<"\n\n";
+
+    // //11. Left Pascal Triangle
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 2*n-i; j > i; j--)
+    //     {
+    //         cout<<" ";
+    //     }
+    //     for (int k = 0; k < i; k++)
+    //     {
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+        
+    // }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < (2*i); j++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     for (int k = n; k > i; k--)
+    //     {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }cout<<"\n\n";
+
+
+    // //12. Damroo Pattern
+
+    
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < i; j++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     for (int k = n; k > i; k--)
+    //     {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = n; j > i; j--)
+    //     {
+    //         cout<<" ";
+    //     }
+    //     for (int k = 0; k < i; k++)
+    //     {
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+
+    // }cout<<"\n\n";
+
+    // // 13. HOLLOW DIAMOND PATTERN
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = n; j > i; j--)
+    //     {
+    //         cout<<" ";
+    //     }
+    //     for (int k = 0;  k < i;  k++)
+    //     {
+    //         if (k==0 || k==(i-1))
+    //         {
+    //             cout<<"* ";
+    //         }
+    //         else
+    //         {
+    //             cout<<"  ";
+    //         }
+            
+    //     }
+    //     cout<<endl;
+        
+        
+    // }
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < i; j++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     for (int k = n; k > i; k--)
+    //     {
+    //         if (k == n || k == (i + 1))
+    //         {
+    //             cout << "* ";
+    //         }
+    //         else
+    //         {
+    //             cout << "  ";
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+
+    // 14.
+
+    
+    // int count=1;
+    // for (int i = 0; i < n; i++)
+    // {
+        
+    //     for (int j = 0; j < i; j++)
+    //     {
+    //         cout <<count++<<" ";
+    //     }
+        
+    //     cout << endl;
+    // }
+
+    //15.
+
+    
+    // for (int i = 1; i <= n; i++)
+    // {
+        
+    //     for (int j = 0; j < i; j++)
+    //     {
+    //         cout <<i<<" ";
+    //     }
+        
+    //     cout << endl;
+    // }
+
+    //16.
+
+    // for (int i = 0; i <= n; i++)
+    // {
+    //     int count =n;
+    //     for (int j = 0; j < i; j++)
+    //     {
+    //         cout <<count--<<" ";
+    //    }
+    //     cout << endl;
+    // }
+
+    //17.
+
+    
+
+        return 0;
+}
