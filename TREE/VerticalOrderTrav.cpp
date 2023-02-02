bd3a4fb68649427cfd335cc3bc5ba5b0cc5afc61
Bhaskar Anand <85471852+BhaskarAnand-05@users.noreply.github.com>
2023-02-02 08:24:20 +0530

    6/365 Contribution

--- TREE/VerticalOrderTrav.cpp
+++ TREE/VerticalOrderTrav.cpp
@@ -1,9 +1,19 @@
 #include"Headerfile.h"
 
 // Vertical Order Traversal
+vector<int> Vertical_Order()
 
 
 int main()
 {
+    Node *tree = new Node(1);
+    tree->left = new Node(2);
+    tree->right = new Node(3);
+    tree->left->left = new Node(4);
+    tree->left->right = new Node(5);
+    tree->right->left = new Node(6);
+    tree->right->right = new Node(7);
+    tree->left->right->right = new Node(8);
+
     return 0;
 }
